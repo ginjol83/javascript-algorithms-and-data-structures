c7a8ef5647e4a5aa1aa1d26b4561a2967d126fe5
agimenez122 <andresgimpe@gmail.com>
2024-01-25 23:10:35 +0100

    more exercices

---
+++ BasicDataStructures/1_UseArrays.js
@@ -0,0 +1,7 @@
+/*
+Use an Array to Store a Collection of Data
+We have defined a variable called yourArray. Complete the statement by assigning an array of at least 5 elements in length to the yourArray variable. 
+Your array should contain at least one string, one number, and one boolean.
+
+*/
+let yourArray =['String',1,true,'sample',2]; // Change this line
